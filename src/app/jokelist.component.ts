71d875c1d93522d853be880f0345418caa14db7f
clodal <joel.koh@me.com>
2018-04-19 10:58:17 +0800

    chapter-4: event binding

--- src/app/jokelist.component.ts
+++ src/app/jokelist.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   <div class="card mb-3" *ngFor="let joke of jokes" style="width: 320px;">
     <div class="card-body">
       <h4 class="card-title">{{joke.setup}}</h4>
-      <p class="card-text">{{joke.punchline}}</p>
+      <p class="card-text" [hidden]="joke.hide">{{joke.punchline}}</p>
+      <button class="btn btn-primary" (click)="toggle(joke)" role="button">Tell me</button>
     </div>
   </div>
 ` })
@@ -17,14 +18,21 @@ export class JokeListComponent {
       {
         setup: 'What did the cheese say when it looked in the mirror?',
         punchline: 'Hello-Me (Halloumi)',
-      }, {
+        hide: true,
+      },
+      {
         setup: 'What kind of cheese do you use to disguise a small horse?',
         punchline: 'Mask-a-pony (Mascarpone)',
+        hide: true,
       },
       {
         setup: 'A kid threw a lump of cheddar at me',
         punchline: 'I thought \'That’s not very mature.\'',
+        hide: true,
       },
     ];
+  }
+  toggle(joke) {
+    joke.hide = !joke.hide;
   }
 }
