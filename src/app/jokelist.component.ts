aef1cbabf56aa33ef9ea283b1604497bb0df35d0
clodal <joel.koh@me.com>
2018-04-19 11:14:39 +0800

    chapter-5: domain model

--- src/app/jokelist.component.ts
+++ src/app/jokelist.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+class Joke {
+  public setup: string;
+  public punchline: string;
+  public hide: boolean;
+  constructor(setup: string, punchline: string) {
+    this.setup = setup;
+    this.punchline = punchline;
+    this.hide = true;
+  }
+  toggle() {
+    this.hide = !this.hide;
+  }
+}
+
 @Component({
   selector: 'joke-list',
   template: `
@@ -7,32 +21,17 @@ import { Component } from '@angular/core';
     <div class="card-body">
       <h4 class="card-title">{{joke.setup}}</h4>
       <p class="card-text" [hidden]="joke.hide">{{joke.punchline}}</p>
-      <button class="btn btn-primary" (click)="toggle(joke)" role="button">Tell me</button>
+      <button class="btn btn-primary" (click)="joke.toggle()" role="button">Tell me</button>
     </div>
   </div>
 ` })
 export class JokeListComponent {
-  jokes: Object[];
+  jokes: Joke[];
   constructor() {
     this.jokes = [
-      {
-        setup: 'What did the cheese say when it looked in the mirror?',
-        punchline: 'Hello-Me (Halloumi)',
-        hide: true,
-      },
-      {
-        setup: 'What kind of cheese do you use to disguise a small horse?',
-        punchline: 'Mask-a-pony (Mascarpone)',
-        hide: true,
-      },
-      {
-        setup: 'A kid threw a lump of cheddar at me',
-        punchline: 'I thought \'That’s not very mature.\'',
-        hide: true,
-      },
+      new Joke('What did the cheese say when it looked in the mirror?', 'Hello-Me (Halloumi)'),
+      new Joke('What kind of cheese do you use to disguise a small horse?', 'Mask-a-pony (Mascarpone)'),
+      new Joke('A kid threw a lump of cheddar at me', 'I thought \'That’s not very mature.\''),
     ];
-  }
-  toggle(joke) {
-    joke.hide = !joke.hide;
   }
 }
