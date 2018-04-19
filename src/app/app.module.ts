95676eb1f4654b1976e0377e0f7ecc188bb5daf2
clodal <joel.koh@me.com>
2018-04-20 02:14:56 +0800

    chapter-8: routing part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { JokeComponent } from './joke.component';
 import { JokeListComponent } from './jokelist.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'jokes', component: JokeListComponent },
+  { path: 'joke-list', redirectTo: 'jokes'},
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
@@ -11,8 +22,13 @@ import { JokeFormComponent } from './joke-form/joke-form.component';
     JokeComponent,
     JokeListComponent,
     JokeFormComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    ),
     BrowserModule
   ],
   providers: [],
